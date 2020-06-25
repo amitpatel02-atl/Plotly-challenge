2cbdd73f6a087a08b2f561bac02100ede04f69ee
amitpatel02-atl <61107007+amitpatel02-atl@users.noreply.github.com>
2020-06-24 22:13:57 -0400

    update code

---
+++ app.js
@@ -0,0 +1,112 @@
+
+var names = [];
+var metadata = [];
+var samples = [];
+
+
+//   Read JSON file
+d3.json("samples.json").then((data)=>{
+
+    //Read data
+    names=data.names;
+    metadata=data.metadata;
+    samples=data.samples;
+
+    console.log(names);
+
+    init();
+});
+
+function init() {
+  // Use D3 to select the dropdown menu
+  var dropdownMenu = d3.select("select");
+  
+  //  Placing all the ID's in drop down menu
+  names.forEach((sample)=>{
+  dropdownMenu.append("option").text(sample).property("value",sample)});
+
+  // Assign the value of the dropdown menu option to a variable
+  var dropValue = dropdownMenu.property("value");
+  console.log(dropValue)
+  
+  demographic_panel(dropValue);
+  create_charts(dropValue);
+  
+}
+
+function demographic_panel(dropValue){
+
+  //   Read JSON file
+  d3.json("samples.json").then((data)=>{
+
+  // Read data
+  metadata=data.metadata;
+
+  var array= metadata.filter(sample_object => sample_object.id == dropValue);
+  var results= array[0];
+
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var panel = d3.select("#sample-metadata");
+  
+    // Use `.html("") to clear any existing metadata
+    panel.html("");
+  
+    // Use `Object.entries` to add each key & value to the panel
+    Object.entries(results).forEach(([key, value]) => {
+      panel.append("h5").text(`${key}:${value}`);
+    });
+  });
+  
+  
+}
+
+function create_charts(sample){
+
+  //   Read JSON file
+  d3.json("samples.json").then((data)=>{
+
+    // Read data
+    samples=data.samples;
+  
+      // We filtered the samples from the samples.json as well as the results
+    var array= samples.filter(sample_object => sample_object.id == sample);
+    var results= array[0];
+    var otu_ids= results.otu_ids;
+    var sample_values= results.sample_values;
+    var otu_labels=results.otu_labels;
+
+  
+    var bubble = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: `markers`,
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Earth'
+      }
+    };
+
+    var data = [bubble];
+    var layout = {
+      title: "Belly Button Bacteria",
+      xaxis: {title: "OTU ID"}
+    };
+    Plotly.newPlot("bubble", data, layout);
+  
+  var bar = [{
+    x: sample_values.slice(0,10).reverse(),
+    y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+    type: "bar",
+    orientation: "h",
+
+  }];
+  Plotly.newPlot("bar", bar);
+});
+};
+
+function optionChanged (sample) {
+demographic_panel(sample);
+create_charts(sample);
+}
